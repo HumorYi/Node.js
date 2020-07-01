1efc287ab6c8ab5c37d59639fada4e346a7605c6
bamboo <HumorYi@users.noreply.github.com>
2020-07-02 03:06:00 +0800

    【新增】cli

---
+++ vue-auto-router-cli/README.md
@@ -0,0 +1,30 @@
+# CLI工具
+
+# 创建工程
+mkdir vue-auto-router-cli
+cd vue-auto-router-cli
+npm init -y
+npm i commander download-git-repo ora handlebars figlet clear chalk open -s
+
+# package.json
+"bin": {
+  "hjy": "./bin/hjy.js"
+},
+
+# bin/hjy.js
+#指定脚本解释器为node
+#!/usr/bin/env node
+console.log('cli.....')
+
+# 定制命令行界面
+hjy.js
+
+# 打印欢迎界面
+/lib/init.js
+
+# 将npm 模块链接到对应的运行项目中去
+npm link
+
+# 删除的情况
+ls /usr/local/bin/
+rm /usr/local/bin/kkb
---
+++ vue-auto-router-cli/lib/init.js
@@ -0,0 +1,50 @@
+const { promisify } = require('util')
+const figlet = promisify(require('figlet'))
+const clear = require('clear')
+const chalk = require('chalk')
+const { clone } = require('./download')
+
+const spawn = async (...args) =>
+  new Promise(resolve => {
+    const { spawn } = require('child_process')
+    const proc = spawn(...args)
+
+    proc.stdout.pipe(process.stdout)
+    proc.stderr.pipe(process.stderr)
+
+    proc.on('close', () => resolve())
+  })
+
+const log = content => console.log(chalk.green(content))
+
+module.exports = async name => {
+  clear()
+
+  const data = await figlet('Welcome')
+
+  log(data)
+
+  log(`🚀创建项目:` + name)
+
+  await clone('github:su37josephxia/vue-template', name)
+
+  log('安装依赖')
+
+  // windows: npm.cmd, linux: npm,  cwd: current work directory
+  await spawn('npm.cmd', ['install'], { cwd: `./${name}` })
+
+  log(
+    chalk.green(`
+👌安装完成：
+To get Start:
+===========================
+  cd ${name}
+  npm run serve
+===========================
+    `)
+  )
+
+  const open = require('open')
+  open('http://localhost:8080')
+  await spawn('npm.cmd', ['run', 'serve'], { cwd: `./${name}` })
+}
